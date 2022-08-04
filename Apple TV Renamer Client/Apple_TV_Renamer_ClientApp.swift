3295b8d35069c34ae3f4d67a887bc271d8ca0fa1
Dalton Durst <d@ltondur.st>
2022-08-04 11:11:08 -0500

    Initial Commit

---
+++ Apple TV Renamer Client/Apple_TV_Renamer_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_TV_Renamer_ClientApp.swift
+//  Apple TV Renamer Client
+//
+//  Created by Dalton Durst on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_TV_Renamer_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple TV Renamer Client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple TV Renamer Client
+//
+//  Created by Dalton Durst on 8/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
